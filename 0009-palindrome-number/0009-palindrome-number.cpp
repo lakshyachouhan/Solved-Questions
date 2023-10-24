869edcdbcb76bf715190c52d5209967914d724e4
lakshyachouhan <120327994+lakshyachouhan@users.noreply.github.com>
2023-10-24 12:04:07 +0530

    Time: 8 ms (55.14%), Space: 6.3 MB (27.19%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -3,11 +3,13 @@ public:
     bool isPalindrome(int x) {
         if(x<0|| (x!=0 &&x%10==0)) return false;
         int sum=0;
-        while(x>sum)
-        {
+        int temp = x;
+        while(x>0)
+        {   if(sum > INT_MAX/10) return false ; 
             sum = sum*10+x%10;
             x = x/10;
         }
-        return (x==sum)||(x==sum/10);
+       cout << sum ;
+        return (temp==sum);
     }
 };
