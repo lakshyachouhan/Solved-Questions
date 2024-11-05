d407f32f0a6aea7f3f5d821c343c19d2448ece31
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2024-11-05 17:46:35 +0530

    Time: 0 ms (100%), Space: 12.3 MB (16.88%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,3 +1,15 @@
+/*
+Jitna easy solution , utnna hi difficult que samajhna hai
+isliye medium kyoki question kuch bol rha hai -> beautiful,partition,even length , minimum
+
+CRUX -> EVEN LENGTH KE CONSECUTIVE 0/1 BANANE HAI 
+USKE LIYE MIN. CHANGES 
+
+HR DO KO CHECK KRNA OPTIMALLY RHEGA , KYOKI 4 KO CONS. BANEKE DEKHE TOH DEPENDENCY PICHE WALE PR 
+
+ISSE ACHA DO DO KE CONSE. GROUPS BANAO 
+*/
+
 class Solution {
 public:
     int minChanges(string s) {
