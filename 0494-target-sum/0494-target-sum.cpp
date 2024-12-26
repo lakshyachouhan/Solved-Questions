0874203f4460a99cacb70c9a5331319e3c871325
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2024-12-26 19:29:58 +0530

    Time: 2415 ms (5.04%), Space: 383.3 MB (5.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+     map<pair<int,int>,int > mp ;
      int solve(vector<int>& nums, int target,int i,int sum){
 
         if(i >= nums.size()){
@@ -7,14 +8,38 @@ public:
             else return 0 ;
         }
 
+        if(mp.find({i,sum}) != mp.end())
+            return mp[{i,sum}];
+
         int add = solve(nums,target,i+1,sum+nums[i]);
         int sub = solve(nums,target,i+1,sum-nums[i]);
 
-        return add + sub ;
+        return mp[{i,sum}] = add + sub ;
+     }
+
+     int solveTab(vector<int>& nums, int target){
+        map<pair<int,int>,int > mp ;
+        mp[{nums.size(),target}] = 1 ;
+
+        for(int i=nums.size()-1; i>=0; i--){
+            for(int sum=-1000; sum<=1000; sum++){
+
+        int add = mp[{i+1,sum+nums[i]}];
+        int sub = mp[{i+1,sum-nums[i]}];
+
+        mp[{i,sum}] = add + sub ;
+
+            }
+        }
+
+        return mp[{0,0}];
      }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        int ans = solve(nums,target,0,0);
+        
+        // int ans = solve(nums,target,0,0);
+
+        int ans = solveTab(nums,target);
         return ans ;
     }
 };
