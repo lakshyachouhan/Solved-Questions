c550a8bacc613b1fc795528cf49571e0391f98d8
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-01-15 00:17:11 +0530

    Time: 125 ms (93.49%), Space: 100.4 MB (81.88%) - LeetHub

--- 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
@@ -1,43 +1,94 @@
 class Solution {
 public:
-    int maximumAmount(vector<vector<int>>& coins) {
-        int m = coins.size(), n = coins[0].size();
-        
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(3, INT_MIN)));
-        
-        dp[0][0][0] = coins[0][0];
-        if (coins[0][0] < 0) dp[0][0][1] = 0;  
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < 3; k++) {
-                    if (dp[i][j][k] == INT_MIN) continue;
-                    
-                    if (j + 1 < n) {
+    // int dp[501][501][3];
 
-                        dp[i][j+1][k] = max(dp[i][j+1][k], dp[i][j][k] + coins[i][j+1]);
+    // int solve(vector<vector<int>>& coins,int i,int j,int limit){
 
-                        if (k < 2 && coins[i][j+1] < 0) {
-                            dp[i][j+1][k+1] = max(dp[i][j+1][k+1], dp[i][j][k]);
+    //     if(i >= coins.size() || j >= coins[0].size())
+    //         return -1e9;
+
+    //     if(i == coins.size()-1 && j == coins[0].size()-1){
+
+    //         if(limit < 2 && coins[i][j] < 0)
+    //             return 0 ;
+
+    //         else return coins[i][j]; 
+    //     }
+
+    //     if( dp[i][j][limit] != -1)
+    //         return  dp[i][j][limit]  ;
+
+    //     // include matlab limit mein +1
+    //     int include = -1e9 ;
+    //     if(coins[i][j] < 0 && limit < 2){
+
+    //         int op1 = 0 + solve(coins,i+1,j,limit+1);
+    //         int op2 = 0 + solve(coins,i,j+1,limit+1);
+
+    //         include = max(op1,op2);
+    //     }
+
+    //     int op3 = coins[i][j] + solve(coins,i+1,j,limit);
+    //     int op4 = coins[i][j] + solve(coins,i,j+1,limit);
+    //     int exclude = max(op3,op4);
+
+    //     return dp[i][j][limit] = max(include,exclude);        
+    // }
+
+    int solveTab(vector<vector<int>>& coins){
+        
+        int m = coins.size() , n = coins[0].size() ;
+        int dp[501][501][3];
+        memset(dp,-1e9,sizeof(dp));
+
+        dp[m-1][n-1][2] = coins[m-1][n-1];
+
+        if(coins[m-1][n-1] < 0){
+            dp[m-1][n-1][0] = dp[m-1][n-1][1] = 0 ;
+        }
+
+        else  dp[m-1][n-1][0] = dp[m-1][n-1][1] = coins[m-1][n-1];
+
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                for(int limit=2; limit>=0; limit--){
+
+                    if(i == m-1 && j == n-1)
+                        continue;
+
+                      int include = -1e9 , exclude = -1e9; ;
+                       if(coins[i][j] < 0 && limit < 2){
+
+                             if(j + 1 < n) 
+                                include = max(include, dp[i][j+1][limit+1]);
+                            
+                            if(i + 1 < m) 
+                                include = max(include, dp[i+1][j][limit+1]);
+                            
                         }
-                    }
-                    
-                    if (i + 1 < m) {
-                        dp[i+1][j][k] = max(dp[i+1][j][k], dp[i][j][k] + coins[i+1][j]);
-                        
-                        if (k < 2 && coins[i+1][j] < 0) {
-                            dp[i+1][j][k+1] = max(dp[i+1][j][k+1], dp[i][j][k]);
-                        }
-                    }
+
+                         if(j + 1 < n) 
+                                exclude = max(exclude, coins[i][j] + dp[i][j+1][limit]);
+                            
+                            if(i + 1 < m) 
+                                exclude = max(exclude, coins[i][j] + dp[i+1][j][limit]);
+                            
+
+                        dp[i][j][limit] = max(include,exclude);        
                 }
             }
         }
-        
 
-        int ans = INT_MIN;
-        for (int k = 0; k < 3; k++) {
-            ans = max(ans, dp[m-1][n-1][k]);
-        }
-        return ans;
+        return dp[0][0][0];       
+
+    }
+
+    int maximumAmount(vector<vector<int>>& coins) {
+        
+        // memset(dp,-1,sizeof(dp));
+        // int ans = solve(coins,0,0,0);
+
+        int ans = solveTab(coins);
+        return ans ;
     }
 };
