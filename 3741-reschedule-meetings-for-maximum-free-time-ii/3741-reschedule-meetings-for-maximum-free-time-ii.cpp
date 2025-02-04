760d44c40bb8497f879a00a830c2d550b5cf0cc7
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-02-04 20:33:48 +0530

    Time: 15 ms (70.74%), Space: 149.6 MB (64.18%) - LeetHub

--- 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
+++ 3741-reschedule-meetings-for-maximum-free-time-ii/3741-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -1,46 +1,66 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
-        int n = startTime.size();
-
-        vector<int> meetTime(n);
-        for(int i = 0; i < n; ++i){
-            meetTime[i] = endTime[i] - startTime[i];
-        }
         
-        vector<int> freeSlot;
-        freeSlot.push_back(startTime[0]);
-        for(int i = 1; i < n; ++i){
-            freeSlot.push_back(startTime[i] - endTime[i - 1]);
-        }
-        freeSlot.push_back(eventTime - endTime[n - 1]);
+        int n = startTime.size();
+        vector<int> meetingTimes(n);
+        vector<int> freeTime(n+1);
 
-        int maxFreeTime = 0;
-        for(int i = 1; i < n + 1; ++i){
-            maxFreeTime = max(maxFreeTime, freeSlot[i - 1] + freeSlot[i]);
+        for(int i=0; i<n; i++){
+
+            meetingTimes[i] = endTime[i] - startTime[i];
         }
 
-        vector<int> pre = freeSlot;
-        vector<int> suf = freeSlot;
-        for(int i = 1; i < n + 1; ++i){
-            pre[i] = max(pre[i], pre[i - 1]);
+        freeTime[0] = startTime[0] - 0 ;
+        freeTime[n] = eventTime - endTime[n-1];
+
+        for(int i=1; i<n; i++){
+
+            freeTime[i] = startTime[i] - endTime[i-1];
         }
-        for(int i = n - 1; i >= 0; --i){
-            suf[i] = max(suf[i], suf[i + 1]);
+
+        // for(auto i:freeTime)
+        //     cout << i << "  " ;
+
+        // first check left , right movement of a single meeting
+        int ans = 0 ;
+        for(int i=0; i<n; i++){
+
+            int curr  = freeTime[i] + freeTime[i+1];
+            ans = max(ans,curr);
+        }
+
+        // maximum freeTime kitna hai iss index tak 
+        vector<int> prefixMax(n+1) , suffixMax(n+1) ;
+        prefixMax[0] = freeTime[0];
+        suffixMax[n] = freeTime[n];
+        for(int i=1; i<n+1; i++){
+
+            prefixMax[i] = max(prefixMax[i-1],freeTime[i]);
+        }
+
+        for(int i=n-1; i>=0; i--){
+
+            suffixMax[i] = max(suffixMax[i+1],freeTime[i]);
         }
 
         auto check = [&](int i){
-            if((i > 0) && (pre[i - 1] >= meetTime[i])) return true;
-            if((i + 2 < n + 1) && (suf[i + 2] >= meetTime[i])) return true;
-            return false;
+
+            if(i-1 >= 0 && prefixMax[i-1] >= meetingTimes[i]) return true ;
+            if(i+2 <= n && suffixMax[i+2] >= meetingTimes[i]) return true ;
+
+            return false ;
         };
 
-        for(int i = 0; i < n; ++i){
+        for(int i=0; i<n; i++){
+
             if(check(i)){
-                maxFreeTime = max(maxFreeTime, freeSlot[i] + meetTime[i] + freeSlot[i + 1]);
+                // ith meeting ko khi bhi move kr skte hai 
+                int curr =  freeTime[i] + meetingTimes[i] + freeTime[i+1];
+                ans = max(ans,curr);
             }
         }
-        
-        return maxFreeTime;    
-    }
+
+        return ans ;
+     }
 };
