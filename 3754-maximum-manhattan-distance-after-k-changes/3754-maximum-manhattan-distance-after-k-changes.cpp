5ad1628a510b1f4054355571b7a9cbfbbd63a836
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-02-10 19:02:28 +0530

    Time: 158 ms (32.83%), Space: 38.8 MB (23.45%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
@@ -1,46 +1,51 @@
 class Solution {
 public:
-    int f(char c) {
-        if (c == 'N') return 0;
-        if (c == 'S') return 1;
-        if (c == 'E') return 2;
-        if (c == 'W') return 3;
-        return -1;
-    }
+    unordered_map<int,char> mp = {{0,'N'},{1,'S'},{2,'E'},{3,'W'}};
     
-    int getAns(vector<int> &cnt, int k) {
-        int res = 0;
-        {
-            int mn = min(cnt[0], cnt[1]);
-            int mx = max(cnt[0], cnt[1]);
-            
-            int del = min(mn, k);
-            mx += del;
-            mn -= del;
-            k -= del;
-            res += mx - mn;
+    int solve(string &s,char xdir, char ydir,int k){
+
+        int x = 0 , y = 0;
+        int ans = 0; 
+        for(int i=0; i<s.size(); i++){
+
+            char ch = s[i];
+
+            if(ch == xdir )    x++;
+            else if(ch == ydir) y++;
+            else if((ydir == 'N' && ch == 'S') || (ydir == 'S' && ch == 'N')){
+
+                if(k){
+                    k-- ;
+                    y++;
+                }
+
+                else y--;
+            }
+
+            else{
+                if(k){
+                    k-- ;
+                    x++;
+                }
+
+                else x--;
+            }
+
+            ans = max(ans,x+y);
         }
-        {
-            int mn = min(cnt[2], cnt[3]);
-            int mx = max(cnt[2], cnt[3]);
-            
-            int del = min(mn, k);
-            mx += del;
-            mn -= del;
-            k -= del;
-            res += mx - mn;
-        }
-        return res;
+
+        return ans ;
     }
-    
+
+
     int maxDistance(string s, int k) {
-        int n = s.size();
-        int ans = 0;
-        vector<int> cnt(4);
-        for (int i = 0; i < n; i++) {
-            cnt[f(s[i])]++;
-            ans = max(ans, getAns(cnt, k));
-        }
-        return ans;
+
+        int ans = 0 ;
+        ans = max(ans,solve(s,'N','E',k));
+        ans = max(ans,solve(s,'S','E',k));
+        ans = max(ans,solve(s,'N','W',k));
+        ans = max(ans,solve(s,'S','W',k));
+
+        return ans ;
     }
 };
