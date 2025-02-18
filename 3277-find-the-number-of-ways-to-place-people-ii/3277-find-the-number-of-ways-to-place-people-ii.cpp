dad762b7e447dc80fbb0f4fa6a7bf01b6fb869e6
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-02-18 20:20:23 +0530

    Time: 133 ms (63%), Space: 41.2 MB (69%) - LeetHub

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/3277-find-the-number-of-ways-to-place-people-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    static bool comp(vector<int>&a,vector<int>& b){
+
+        if(a[0] == b[0])
+            return a[1] > b[1];
+
+        return a[0] < b[0];
+    }
+
+    int numberOfPairs(vector<vector<int>>& points) {
+        
+        sort(points.begin(),points.end(),comp);
+        // x in increasing order 
+        // if x is same , then y in decreasing 
+        // graph mein plot krke dekho 
+
+        int ans = 0 ;
+
+        for(int i=0; i<points.size(); i++){
+
+            int cx = points[i][0] , cy = points[i][1] ; 
+            int limitY = -100000000000 ;  // constraints -1e10
+
+            for(int j=i+1; j<points.size(); j++){
+
+                int tx = points[j][0] , ty = points[j][1];
+
+                if(cy < ty) continue ;
+                if(limitY < ty){
+                    ans++;
+                    limitY = ty ;
+                }
+
+            }
+
+        }
+
+        return ans ;
+    }
+};
