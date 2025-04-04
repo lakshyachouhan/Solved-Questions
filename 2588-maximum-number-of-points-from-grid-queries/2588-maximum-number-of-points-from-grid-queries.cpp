1e0504ac31961f74cd61f727ff6412092aa2c389
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-04-05 00:12:14 +0530

    Time: 99 ms (44.58%), Space: 47.1 MB (29.13%) - LeetHub

---
+++ 2588-maximum-number-of-points-from-grid-queries/2588-maximum-number-of-points-from-grid-queries.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<int> maxPoints(vector<vector<int>>& grid, vector<int>& queries) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+
+        vector<pair<int,int>> sortedQ ; // {ele,index}
+        for(int i=0; i<queries.size(); i++){
+            sortedQ.push_back({queries[i],i});
+        }
+
+        sort(sortedQ.begin(),sortedQ.end());
+
+        vector<int> ans(queries.size());
+        int points = 0 ;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> > minHeap ; // {ele,{i,j}}
+
+        minHeap.push({grid[0][0],{0,0}});
+
+        vector<vector<bool>> vis(m,vector<bool>(n,0));
+        vis[0][0] = 1 ;
+
+        int dx[] = {-1,0,1,0}; 
+        int dy[] = {0,1,0,-1};
+
+        for(int i=0; i<queries.size(); i++){
+
+            int val = sortedQ[i].first ;
+            int idx = sortedQ[i].second ;
+
+            // visit all smalller value cells 
+            while(!minHeap.empty() && minHeap.top().first < val){
+
+                auto top = minHeap.top();
+                int x = top.second.first ;
+                int y = top.second.second ;
+                minHeap.pop();
+
+                points++;
+
+                for(int i=0; i<4; i++){
+
+                    int newX = dx[i] + x ;
+                    int newY = dy[i] + y ;
+
+                    if(newX>=0 && newY>=0 && newX < m && newY <n){
+
+                        if(!vis[newX][newY]){
+                            vis[newX][newY] = 1 ;
+                            minHeap.push({grid[newX][newY],{newX,newY}});
+                        }
+                    }
+                }
+            }
+
+            ans[idx] = points; 
+        }
+
+        return ans ;        
+    }
+};
