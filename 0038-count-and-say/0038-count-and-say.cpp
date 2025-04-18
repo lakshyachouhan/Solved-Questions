17628c0bd94f9729c43574b37e18b8e9ecf34a92
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-04-18 10:27:49 +0530

    Time: 0 ms (100%), Space: 9.9 MB (72.29%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,30 +1,66 @@
 class Solution {
 public:
+    string solveIteratively(int n){
+
+        if(n == 1)
+            return "1" ;
+
+        string curr = "1" ;
+
+        for(int i=2; i<=n; i++){
+
+            string ans = "";
+
+            char ch = curr[0];
+            int count = 1 ;
+            for(int j=1; j<curr.size(); j++){
+
+                if(curr[j] == ch)
+                    count++;
+
+                else{
+                    // previous insert , and new start 
+                    ans += to_string(count) + ch ; 
+                    count = 1 ;
+                    ch = curr[j];
+                }
+            }
+
+            ans += to_string(count) + ch ; 
+            curr = ans ;
+        }
+
+        return curr ;
+    }
+
+
     string countAndSay(int n) {
         
         if(n == 1)
             return "1" ;
 
-        string curr = countAndSay(n-1);
-        string ans = "";
+        // string curr = countAndSay(n-1);
+        // string ans = "";
 
-        char ch = curr[0];
-        int count = 1 ;
-        for(int j=1; j<curr.size(); j++){
+        // char ch = curr[0];
+        // int count = 1 ;
+        // for(int j=1; j<curr.size(); j++){
 
-            if(curr[j] == ch)
-                count++;
+        //     if(curr[j] == ch)
+        //         count++;
 
-            else{
-                // previous insert , and new start 
-                ans += to_string(count) + ch ; 
-                count = 1 ;
-                ch = curr[j];
-            }
-        }
+        //     else{
+        //         // previous insert , and new start 
+        //         ans += to_string(count) + ch ; 
+        //         count = 1 ;
+        //         ch = curr[j];
+        //     }
+        // }
 
-        ans += to_string(count) + ch ; 
+        // ans += to_string(count) + ch ; 
 
-        return ans ;
+        // return ans ;
+
+        return solveIteratively(n);
     }
 };
