2b87aa5e03382f6f77ae32d6dd2b638a4085fbff
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-05-10 12:50:50 +0530

    Time: 135 ms (40.68%), Space: 135.4 MB (50.17%) - LeetHub

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -11,8 +11,8 @@ public:
         ll sum2 = accumulate(nums2.begin(),nums2.end(),0LL);
         ll z2 = count(nums2.begin(),nums2.end(),0);
 
-        cout << sum1 << "  " << z1 << endl ;
-        cout << sum2 << "  " << z2 << endl ;
+        // cout << sum1 << "  " << z1 << endl ;
+        // cout << sum2 << "  " << z2 << endl ;
 
         if(sum1 == sum2){
 
@@ -28,7 +28,7 @@ public:
         else if(sum1 > sum2){
 
             ll diff = sum1 - sum2 ;
-            diff += z1 ;
+            diff += z1 ;  // jitne z1 the unko 1 man liya hai 
 
             if(diff >= z2 && z2 != 0)
                 return sum2 + diff ;
@@ -42,7 +42,7 @@ public:
         else{
 
             ll diff = sum2 - sum1 ;
-            diff += z2 ;
+            diff += z2 ; // jitne z2 the unko 1 man liya hai 
 
             if(diff >= z1 && z1 != 0 )
                 return sum1 + diff ;
