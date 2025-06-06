c7a6e522b7d534d82cec0b3a3c198e41a872505e
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-06-07 00:11:35 +0530

    Time: 111 ms (21.78%), Space: 34.9 MB (36.63%) - LeetHub

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string robotWithString(string s) {
+        string ans = "";
+        string t = "";
+        
+        unordered_map<char, int> frq;
+        for (char c : s) {
+            frq[c]++;
+        }
+
+        char sml = 'a';
+
+        for (char c : s) {
+
+            // if current char is smallest, push to ans directly
+            if (c == sml)   ans += c;
+            else    t.push_back(c);
+
+            frq[c]--;
+            if (frq[c] == 0)
+                frq.erase(c);
+
+            // update sml to next smallest available char
+            while (sml <= 'z' && frq.count(sml) == 0) {
+                sml++;
+            }
+
+            // pop from t if top <= sml
+            while (!t.empty() && t.back() <= sml) {
+                ans += t.back();
+                t.pop_back();
+            }
+        }
+
+        return ans;
+    }
+};
