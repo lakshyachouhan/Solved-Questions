b0b1d33490119978f8e8b2e65080454c68a6d878
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-08-04 18:41:52 +0530

    Time: 28 ms (87.42%), Space: 75.1 MB (31.99%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        
+        // longest subarray containing exactly two elements 
+
+        unordered_map<int,int> mp ;
+        int i = 0 , j = 0 ;
+        int ans = -1 ;
+
+        while(j < fruits.size()){
+
+            mp[fruits[j]]++;
+
+            while(mp.size() > 2){
+                
+                mp[fruits[i]]--;
+                if(mp[fruits[i]] == 0)
+                    mp.erase(fruits[i]);
+
+                i++;
+            }
+
+            ans = max(ans,j-i+1);
+            j++;
+        }
+
+        return ans ;
+    }
+};
