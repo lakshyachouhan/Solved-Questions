a97619a29742fda585f4d9761827db36f3eadd89
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-09-04 22:13:05 +0530

    Time: 0 ms (100%), Space: 8.5 MB (55.11%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        
+        int diff1 = abs(x-z) , diff2 = abs(y-z);
+
+        if(diff1 == diff2)
+            return 0 ;
+
+        else if(diff1 < diff2)
+            return 1 ;
+
+        else return 2 ;
+    }
+};
