3f1db5ac049d3af704defdb57bde4d8310a32d44
Lakshya Chouhan <120327994+lakshyachouhan@users.noreply.github.com>
2025-10-19 22:46:29 +0530

    Time: 204 ms (69.91%), Space: 102.9 MB (30.97%) - LeetHub

---
+++ 1747-lexicographically-smallest-string-after-applying-operations/1747-lexicographically-smallest-string-after-applying-operations.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string findLexSmallestString(string s, int a, int b) {
+
+        unordered_set<string> vis;
+        string ans = s;
+        queue<string> q;
+        q.push(s);
+        vis.insert(s);
+
+        while (!q.empty()) {
+
+            string cur = q.front(); q.pop();
+
+            if (cur < ans) ans = cur;
+
+            string added = cur;
+            for (int i = 1; i < added.size(); i += 2)
+                added[i] = ((added[i] - '0' + a) % 10) + '0';
+
+            if (!vis.count(added)) {
+                vis.insert(added);
+                q.push(added);
+            }
+
+            string rotated = cur.substr(cur.size() - b) + cur.substr(0, cur.size() - b);
+
+            if (!vis.count(rotated)) {
+                vis.insert(rotated);
+                q.push(rotated);
+            }
+        }
+        
+        return ans;
+    }
+};
